5e26c7551b0ecadd34cac7c3d4d3e7cc27e07686
Michael Rentsch <64227211+Michael-Rentsch@users.noreply.github.com>
2023-11-16 21:49:05 +0200

    Image caching downloading and project organiszation

--- Appetizers/Views/Cells/AppetizerListCell.swift
+++ Appetizers/Views/Cells/AppetizerListCell.swift
@@ -13,15 +13,16 @@ struct AppetizerListCell: View {
     
     var body: some View {
         HStack {
-            Image("juicy-steak-medium-rare-beef-with-spices-grilled-vegetables")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 120, height: 80)
+                .frame(width: 120, height: 90)
                 .cornerRadius(8)
+            
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
                     .font(.title2)
                     .fontWeight(.medium)
+                
                 Text("$\(appetizer.price, specifier: "%.2f")")
                     .foregroundColor(.secondary)
                     .fontWeight(.semibold)
